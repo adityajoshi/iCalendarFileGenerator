988d559b3fc25ee54159b92c48c403612bb65a95
adityajoshi <aditya.joshi1@outlook.com>
2023-06-09 08:17:29 +0530

    Update csvToIcs.py

--- csvToIcs.py
+++ csvToIcs.py
@@ -1,11 +1,6 @@
-# Script to create iCal file (ics) from a input file.
-# This input file should be semicolon separated.
+# File - csvToIcs.py
+# My Simple tool to create iCalendar file from given list of events.
 
-
-# Sample Input file format :
-# Subject;Start Timestamp;End Timestamp;Location;Description
-# Really Cool Event;20230530T1000Z;20230530T1100Z;"Mumbai, India";Cool Event
-# Awesome event;20230531T1000Z;20230531T1100Z;"Mumbai, India";Excellent Event
 
 import csv
 import hashlib
